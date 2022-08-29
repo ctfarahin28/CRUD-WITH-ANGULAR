6dc1ddcdf55f80938c9aff3a6a88fd9eba49dce8
ctfarahin28 <ctfarahin28@gmail.com>
2022-08-29 16:32:00 +0800

    2 files modified

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -1,47 +1,50 @@
-
-import {User} from './user-data';
+import { User } from './user-data';
 //import { Details } from './ProductDetails';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { UserFetch } from './user-fetch';
 
-
 export class UserData implements InMemoryDbService {
-  createDb(){
+  createDb() {
     const users: User[] = [
-      { id: 1, name: 'Pen', model: 'v1', price: '0000000000'  },
-      { id: 2, name: 'Pencil', model: 'A3', price: '1111111111'  },
-      { id: 3, name: 'Table', model: 'N8', price: '2222222222'  },
-      { id: 4, name: 'Chair', model: 'M9', price : '6666666666' },
-      { id: 5, name: 'Bed', model: 'PZ', price: '9909999999'  }
-
+      { id: 1, name: 'Pen', model: 'v1', price: '0000000000' },
+      { id: 2, name: 'Pencil', model: 'A3', price: '1111111111' },
+      { id: 3, name: 'Table', model: 'N8', price: '2222222222' },
+      { id: 4, name: 'Chair', model: 'M9', price: '6666666666' },
+      { id: 5, name: 'Bed', model: 'PZ', price: '9909999999' },
+      { id: 6, name: 'Status', model: 'PZ', price: '9909999999' },
     ];
-    const user: UserFetch[]=[
+    const user: UserFetch[] = [
       {
-        id:1,
+        id: 1,
         name: 'Pen',
-        model: 'AB'
+        model: 'AB',
       },
       {
-       id:2,
-       name: 'Pencil',
-       model: 'CD'
-     }    ,
-     {
-       id:3,
-       name: 'Table',
-       model: 'FG'
-     }    ,
-     {
-       id:4,
-       name: 'Chair',
-       model: 'IJ'
-     } ,
-     {
-       id:5,
-       name: 'Bed',
-       model: 'XZ'
-     }
-     ];
+        id: 2,
+        name: 'Pencil',
+        model: 'CD',
+      },
+      {
+        id: 3,
+        name: 'Table',
+        model: 'FG',
+      },
+      {
+        id: 4,
+        name: 'Chair',
+        model: 'IJ',
+      },
+      {
+        id: 5,
+        name: 'Bed',
+        model: 'XZ',
+      },
+      {
+        id: 6,
+        name: 'Status',
+        model: 'XZ',
+      },
+    ];
 
     // const details: Details[]=[
     //  {
@@ -84,6 +87,6 @@ export class UserData implements InMemoryDbService {
 
     // }
     // ];
-    return {users, user};
+    return { users, user };
   }
 }
